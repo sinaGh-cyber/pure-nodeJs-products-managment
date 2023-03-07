112ddb65329cbb16b6094be258201fdcf44739db
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2023-03-07 14:15:29 +0330

    modification: view leare completed

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const http = require('http');
+const ErrorHandler = require('./controllers/errorHandler.controller');
 const ProductsController = require('./controllers/product.controllers');
 const PORT = 3000;
 const products = require('./data/products.json');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  console.log(`${req.method}: ${req.url}`);
+  const apiRout = 'api';
+  const productsRout = `/${apiRout}/products`;
+  const singleProductRout = /\/api\/products\/[0-9]+/;
+  const { url, method } = req;
 
-  if (req.url === '/api/products') {
+  if (url === productsRout && method === 'GET') {
     ProductsController.get(req, res);
+  } else if (url.match(singleProductRout) && method === 'GET') {
+    ProductsController.getById(req, res);
+  } else if (url.match(singleProductRout) && method === 'POST') {
+    ProductsController.create(req, res);
+  } else if (url.match(singleProductRout) && method === 'PUT') {
+    ProductsController.update(req, res);
+  } else if (url.match(singleProductRout) && method === 'DELETE') {
+    ProductsController.remove(req, res);
   } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({ message: 'Rout Not Found' }));
-    res.end();
+    ErrorHandler.notFound(res);
   }
 });
 
