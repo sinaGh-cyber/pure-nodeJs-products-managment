1898e57ba5ae84b9846d4f04d4d1bb4a59325ba2
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2023-03-07 14:15:09 +0330

    modification: controlle leare completed

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -1,5 +1,55 @@
 const ProductModel = require('../model/product.model');
 
+async function create(req, res) {
+  try {
+    let body = '';
+
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on('end', async () => {
+      const product = { id: Date.now(), ...JSON.parse(body) };
+      const result = await ProductModel.create(product);
+
+      res.writeHead(201, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify(result));
+      res.end();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function update(req, res) {
+  try {
+    let body = '';
+    const id = req.url.split('/')[3];
+    const product = await ProductModel.findById(id);
+
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on('end', async () => {
+      const parsedBody = { ...JSON.parse(body) };
+
+      if (!product) {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ message: 'product not found' }));
+        res.end();
+      } else {
+        const result = await ProductModel.update(id, parsedBody);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(result));
+        res.end();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 async function get(req, res) {
   try {
     const products = await ProductModel.find();
@@ -7,7 +57,44 @@ async function get(req, res) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(products));
     res.end();
-    
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getById(req, res) {
+  try {
+    const id = req.url.split('/')[3];
+    const product = await ProductModel.findById(id);
+    if (product) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify(product));
+      res.end();
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify({ message: 'product not found' }));
+      res.end();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function remove(req, res) {
+  try {
+    const id = req.url.split('/')[3];
+    const product = await ProductModel.findById(id);
+    if (!product) {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify({ message: 'product not found' }));
+      res.end();
+    } else {
+      const result = await ProductModel.remove(id)
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(JSON.stringify(result));
+      res.end();
+    }
   } catch (error) {
     console.log(error);
   }
@@ -15,6 +102,10 @@ async function get(req, res) {
 
 const ProductsController = {
   get,
+  getById,
+  create,
+  update,
+  remove
 };
 
 module.exports = ProductsController;
